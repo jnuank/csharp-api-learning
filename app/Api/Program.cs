ead90af2a1b766fd7ff98ecce837af14aea6ca80
jnuank <ikuta1919@gmail.com>
2025-12-29 00:32:42 +0900

    Dockerfileつくった。dotnet publishが良いらしい

--- app/Api/Program.cs
+++ app/Api/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 3).Select(index =>
+    var forecast = Enumerable.Range(1, 9).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
