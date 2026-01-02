ab5cb87fdfe9952591bd5bdc9b6040499e2c9c39
jnuank <ikuta1919@gmail.com>
2026-01-02 12:21:31 +0900

    tidy

--- app/Api/Program.cs
+++ app/Api/Program.cs
@@ -66,9 +66,3 @@ app.MapGet("exception", () => {
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    public static string TestName => "Test";
-}
